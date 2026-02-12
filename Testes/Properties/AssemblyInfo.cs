0afd2a6fef7453511b890b5973aed93c7d6d5b07
lealthony <anthonyfuzinato@gmail.com>
2026-02-12 08:33:18 +0000

    12/02

---
+++ Testes/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Testes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Testes")]
+[assembly: AssemblyCopyright("Copyright © HP 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b3bc452a-5876-463a-9473-b573a1535cec")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Testes/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using M17E_intro.Classes;
+namespace Testes
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        // testa se o login devolve verdadeiro
+        [TestMethod]
+        public void TestaLoginVerdadeiro()
+        {
+            Utilizadores utilizadores = new Utilizadores();
+            utilizadores.email = "admin@gmail.com";
+            utilizadores.palavra_passe = "12345";
+            Assert.IsTrue(utilizadores.VerificaLogin()); 
+        }
+        // teste se o login devolve falso
+        [TestMethod]
+        public void TestaLoginFalso()
+        {
+            Utilizadores utilizadores = new Utilizadores();
+            utilizadores.email = "seila@gmail.com";
+            utilizadores.palavra_passe = "12345";
+            Assert.IsFalse(utilizadores.VerificaLogin());
+        }
+        // teste se o login com admin maiscula devolve verdadeiro
+        [TestMethod]
+        public void TestaLoginVerdadeiroComMaiscula()
+        {
+            Utilizadores utilizadores = new Utilizadores();
+            utilizadores.email = "admin@gmail.com";
+            utilizadores.palavra_passe = "12345";
+            Assert.IsTrue(utilizadores.VerificaLogin());
+        }
+    }
+}
